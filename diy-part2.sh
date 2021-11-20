c7ab18fc282fc613a700cafb56a9e9fc6cc9b1e9
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2021-11-20 14:34:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -45,7 +45,7 @@ git clone https://github.com/QiuSimons/openwrt-mos package/luci-app-mosdns
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 
 # 编译 po2lmo (如果有po2lmo可跳过)
-pushd luci-app-openclash/tools/po2lmo
+pushd package/luci-app-openclash/tools/po2lmo
 make && sudo make install
 popd
 
