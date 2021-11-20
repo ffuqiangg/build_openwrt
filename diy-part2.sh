da8cdb98ef5eaf19f9302ecd943ec84d516e558a
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2021-11-20 14:28:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -42,6 +42,12 @@ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy package/na
 git clone https://github.com/immortalwrt-collections/openwrt-filebrowser package/luci-app-filebrowser
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 git clone https://github.com/QiuSimons/openwrt-mos package/luci-app-mosdns
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
 
 # Set DISTRIB_REVISION
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
