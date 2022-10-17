0c141f1fd4c170597f5097bb5c8a1fd0d13992f0
ffuqiangg <ffuqiangg@gmail.com>
2022-10-17 21:19:13 +0800

    modify comment

--- diy-part2.sh
+++ diy-part2.sh
@@ -39,7 +39,7 @@ git clone --single-branch --depth=1 https://github.com/xiaorouji/openwrt-passwal
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
 git clone --single-branch --depth=1 https://github.com/sbwml/luci-app-alist package/alist
 
-# golang
+# Dump golang version
 sed -i 's/GO_VERSION_MAJOR_MINOR:=.*/GO_VERSION_MAJOR_MINOR:=1.19/g' feeds/packages/lang/golang/golang/Makefile
 sed -i 's/GO_VERSION_PATCH:=.*/GO_VERSION_PATCH:=2/g' feeds/packages/lang/golang/golang/Makefile
 sed -i 's/PKG_HASH:=.*/PKG_HASH:=2ce930d70a931de660fdaf271d70192793b1b240272645bf0275779f6704df6b/g' feeds/packages/lang/golang/golang/Makefile
