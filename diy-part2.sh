444cbdde685761a937057a4de3c5e6d18abc731c
ffuqiangg <ffuqiangg@gmail.com>
2022-11-19 22:40:13 +0800

    modify vimrc

--- diy-part2.sh
+++ diy-part2.sh
@@ -110,7 +110,7 @@ sed -i "s|OPENWRT_VERSION|R$(date +%y.%m.%d)|g" package/lean/default-settings/fi
 # Modify vimrc
 cp -f ${GITHUB_WORKSPACE}/general/vim/molokai.vim package/base-files/files/etc/
 sed -i '/exit/i\mv /etc/molokai.vim /usr/share/vim/vim??/colors/\n' package/lean/default-settings/files/zzz-default-settings
-sed -i -e '1i colorscheme molokai\n' -e '/autoindent/d' feeds/packages/utils/vim/files/vimrc.full
+sed -i '1i colorscheme molokai\n' feeds/packages/utils/vim/files/vimrc.full
 cat >> feeds/packages/utils/vim/files/vimrc.full <<EOF
 set tabstop=4
 set shiftwidth=4
@@ -120,7 +120,6 @@ set number
 set cursorline
 set nowrap
 set sidescroll=1
-set smartindent
 
 " Auto ([{
 inoremap ( ()<Esc>i
