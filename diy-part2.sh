d67565f162179a66bfbc1c23ab3832ee6f2ed211
ffuqiangg <ffuqiangg@gmail.com>
2022-12-05 15:43:11 +0800

    modiy menu & add alias

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,23 +17,16 @@ sed -i 's/192.168.1.1/192.168.1.99/g' package/base-files/files/bin/config_genera
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
 
 # Modify menu
-startLine=$(cat package/lean/default-settings/files/zzz-default-settings | grep -n services | head -1 | cut -d : -f 1)
 sed -i '/services/d' package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/\\\\\"nas\\\\\"/\\\\\"services\\\\\"/g' /usr/lib/lua/luci/controller/vsftpd.lua" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/\\\\\"NAS\\\\\"/\\\\\"Services\\\\\"/g' /usr/lib/lua/luci/controller/vsftpd.lua" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/\\\\\"system\\\\\"/\\\\\"services\\\\\"/g' /usr/lib/lua/luci/controller/cpufreq.lua" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/\\\\\"nas\\\\\"/\\\\\"services\\\\\"/g' /usr/lib/lua/luci/controller/filebrowser.lua" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/\\\\\"NAS\\\\\"/\\\\\"Services\\\\\"/g' /usr/lib/lua/luci/controller/filebrowser.lua" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/nas/services/g' /usr/lib/lua/luci/view/filebrowser/status.htm" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/nas/services/g' /usr/lib/lua/luci/view/filebrowser/log.htm" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/nas/services/g' /usr/lib/lua/luci/view/filebrowser/download.htm" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/\\\\\"nas\\\\\"/\\\\\"services\\\\\"/g' /usr/lib/lua/luci/controller/alist.lua" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/\\\\\"NAS\\\\\"/\\\\\"Services\\\\\"/g' /usr/lib/lua/luci/controller/alist.lua" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/nas/services/g' /usr/lib/lua/luci/view/alist/admin_info.htm" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/nas/services/g' /usr/lib/lua/luci/view/alist/alist_log.htm" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/nas/services/g' /usr/lib/lua/luci/view/alist/alist_status.htm" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/\\\\\"nas\\\\\"/\\\\\"services\\\\\"/g' /usr/lib/lua/luci/controller/rclone.lua" package/lean/default-settings/files/zzz-default-settings
-sed -i "${startLine}i\sed -i 's/\\\\\"NAS\\\\\"/\\\\\"Services\\\\\"/g' /usr/lib/lua/luci/controller/rclone.lua" package/lean/default-settings/files/zzz-default-settings
+# vsftpd
+sed -i -e 's/\"nas\"/\"services\"/g' -e 's/\"NAS\"/\"Services\"/g' package/feeds/luci/luci-app-vsftpd/luasrc/controller/vsftpd.lua
+sed -i 's/nas/services/g' package/feeds/luci/luci-app-vsftpd/luasrc/model/cbi/vsftpd/item.lua
+sed -i 's/nas/services/g' package/feeds/luci/luci-app-vsftpd/luasrc/model/cbi/vsftpd/users.lua
+# cpufreq
+sed -i 's/\"system\"/\"services\"/g' package/feeds/luci/luci-app-cpufreq/luasrc/controller/cpufreq.lua
+# rclone
+sed -i -e 's/\"NAS\"/\"Services\"/g' -e 's/\"nas\"/\"services\"/g' package/feeds/luci/luci-app-rclone/luasrc/controller/rclone.lua
+# dockerman
 sed -i -e 's/admin\",/& \"services\",/g' -e 's/Docker/&Man/' package/feeds/luci/luci-app-dockerman/luasrc/controller/dockerman.lua
 sed -i 's/admin\//&services\//g' package/feeds/luci/luci-app-dockerman/luasrc/model/cbi/dockerman/container.lua
 sed -i 's/admin\//&services\//g' package/feeds/luci/luci-app-dockerman/luasrc/model/cbi/dockerman/containers.lua
@@ -62,6 +55,14 @@ alias ll='ls -alhF --color=auto'
 alias la='ls -A'
 alias l='ls -CF'
 alias clr='clear'
+
+# Alias's for archives
+alias mktar='tar -cvf'
+alias mkbz2='tar -cvjf'
+alias mkgz='tar -cvzf'
+alias untar='tar -xvf'
+alias unbz2='tar -xvjf'
+alias ungz='tar -xvzf'
 
 # Change directory aliases
 alias home='cd ~'
@@ -144,11 +145,23 @@ filetype on
 autocmd Filetype yaml setlocal tabstop=2 shiftwidth=2 softtabstop=2
 EOF
 
-# Add applications
+# Add passwall
 git clone --single-branch -b luci --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 git clone --single-branch --depth=1 https://github.com/xiaorouji/openwrt-passwall.git  package/passwall-depends
+
+# Add filebrowser & change menu
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
+sed -i -e 's/nas/services/g' -e 's/NAS/Services/g' package/luci-app-filebrowser/luasrc/controller/filebrowser.lua
+sed -i 's/nas/services/g' package/luci-app-filebrowser/luasrc/view/filebrowser/download.htm
+sed -i 's/nas/services/g' package/luci-app-filebrowser/luasrc/view/filebrowser/log.htm
+sed -i 's/nas/services/g' package/luci-app-filebrowser/luasrc/view/filebrowser/status.htm
+
+# Add luci-app-alist & change menu
 git clone --single-branch --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
+sed -i -e 's/nas/services/g' -e 's/NAS/Services/g' package/luci-app-alist/luci-app-alist/luasrc/controller/alist.lua
+sed -i 's/nas/services/g' package/luci-app-alist/luci-app-alist/luasrc/view/alist/admin_info.htm
+sed -i 's/nas/services/g' package/luci-app-alist/luci-app-alist/luasrc/view/alist/alist_log.htm
+sed -i 's/nas/services/g' package/luci-app-alist/luci-app-alist/luasrc/view/alist/alist_status.htm
 
 # Dump golang version
 sed -i 's/GO_VERSION_MAJOR_MINOR:=.*/GO_VERSION_MAJOR_MINOR:=1.19/g' feeds/packages/lang/golang/golang/Makefile
