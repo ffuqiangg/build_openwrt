eca40b5a3bdf4a8454fa57be675d7c6200ab3a34
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2022-12-12 11:24:24 +0800

    Update immortalwrt-18.06-diy2.sh

--- immortalwrt-18.06-diy2.sh
+++ immortalwrt-18.06-diy2.sh
@@ -163,6 +163,3 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 sed -i 's/GO_VERSION_MAJOR_MINOR:=.*/GO_VERSION_MAJOR_MINOR:=1.19/g' feeds/packages/lang/golang/golang/Makefile
 sed -i 's/GO_VERSION_PATCH:=.*/GO_VERSION_PATCH:=2/g' feeds/packages/lang/golang/golang/Makefile
 sed -i 's/PKG_HASH:=.*/PKG_HASH:=2ce930d70a931de660fdaf271d70192793b1b240272645bf0275779f6704df6b/g' feeds/packages/lang/golang/golang/Makefile
-
-./scripts/feeds update -a
-./scripts/feeds install -a
