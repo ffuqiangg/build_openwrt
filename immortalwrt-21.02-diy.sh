c4b5a44abeb7e30fd0406de657d1a66b9987766d
ffuqiangg <ffuqiangg@gmail.com>
2023-02-05 22:42:42 +0800

    modify vim

--- immortalwrt-21.02-diy.sh
+++ immortalwrt-21.02-diy.sh
@@ -72,11 +72,11 @@ EOF
 
 # Modify vim
 cp -f ${GITHUB_WORKSPACE}/general/vim/vimrc packages/utils/vim/files/vimrc.full
-# cp -f ${GITHUB_WORKSPACE}/general/vim/colors/onedark.vim package/base-files/files/etc/colors.vim
-# cp -f ${GITHUB_WORKSPACE}/general/vim/autoload/onedark.vim package/base-files/files/etc/autoload.vim
-# sed -i '/exit/i\mv /etc/colors.vim /usr/share/vim/vim??/colors/onedark.vim\
-# mv /etc/autoload.vim /usr/share/vim/vim??/autoload/onedark.vim\
-# ' package/lean/default-settings/files/zzz-default-settings
+cp -f ${GITHUB_WORKSPACE}/general/vim/colors/yowish.vim package/base-files/files/etc/colors.vim
+cp -f ${GITHUB_WORKSPACE}/general/vim/autoload/yowish.vim package/base-files/files/etc/autoload.vim
+sed -i '/exit/i\mv /etc/colors.vim /usr/share/vim/vim*[0-9]*/colors/yowish.vim\
+mv /etc/autoload.vim /usr/share/vim/vim*[0-9]*/autoload/yowish.vim\
+' package/lean/default-settings/files/zzz-default-settings
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
