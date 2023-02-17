eeb25c08b1031581c6c132781d0627be70d98a88
ffuqiangg <ffuqiangg@gmail.com>
2023-02-17 17:00:13 +0800

    simplify vimrc

--- lede-diy2.sh
+++ lede-diy2.sh
@@ -122,11 +122,11 @@ sed -i "s|OPENWRT_VERSION|R$(date +%y.%m.%d)|g" package/lean/default-settings/fi
 
 # Modify vim
 cp -f ${GITHUB_WORKSPACE}/general/vim/vimrc feeds/packages/utils/vim/files/vimrc.full
-cp -f ${GITHUB_WORKSPACE}/general/vim/colors/*.vim package/base-files/files/etc/
-sed -i '/exit/i\mv /etc/*.vim /usr/share/vim/VIMXX/colors/\
-' package/lean/default-settings/files/zzz-default-settings
-vimver=$(awk -F "=" '/VIMVER:/ {print $2}' feeds/packages/utils/vim/Makefile)
-sed -i "s|VIMXX|vim${vimver}|" package/lean/default-settings/files/zzz-default-settings
+# cp -f ${GITHUB_WORKSPACE}/general/vim/colors/*.vim package/base-files/files/etc/
+# sed -i '/exit/i\mv /etc/*.vim /usr/share/vim/VIMXX/colors/\
+# ' package/lean/default-settings/files/zzz-default-settings
+# vimver=$(awk -F "=" '/VIMVER:/ {print $2}' feeds/packages/utils/vim/Makefile)
+# sed -i "s|VIMXX|vim${vimver}|" package/lean/default-settings/files/zzz-default-settings
 
 # Add passwall
 git clone --single-branch -b luci --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
