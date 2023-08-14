5a7f36438c59b5153d4c46229d35339b2a2bac99
ffuqiangg <ffuqiangg@gmail.com>
2023-08-14 19:46:20 +0800

    remake repo

--- config/immortalwrt-master/diy.sh
+++ config/immortalwrt-master/diy.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
