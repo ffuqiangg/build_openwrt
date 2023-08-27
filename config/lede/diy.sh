628fd406bed4e42a588639417a94086c08702c88
ffuqiangg <ffuqiangg@gmail.com>
2023-08-27 23:57:42 +0800

    modify banner

--- config/lede/diy.sh
+++ config/lede/diy.sh
@@ -43,10 +43,10 @@ sed -i 's/admin\//&network\//g' package/feeds/luci/luci-app-nlbwmon/luasrc/view/
 sed -i 's/admin\//&network\//g' package/feeds/luci/luci-app-nlbwmon/luasrc/view/nlbw/display.htm
 
 # Change banner
-sed -i '/exit/i\echo "╭─────────────────────╮" > /etc/banner\
-echo "│[36m  ╷   ┌─┐   ▁╷  ┌─┐  [0m│" >> /etc/banner\
-echo "│[36m  └─  └─   └─┘  └─   [0m│" >> /etc/banner\
-echo "╰─────────────────────╯" >> /etc/banner\
+sed -i '/exit/i\echo "╭──────────────────╮" > /etc/banner\
+echo "│[36m  ╷  ┌─┐  ▁╷ ┌─┐  [0m│" >> /etc/banner\
+echo "│[36m  └─ └─  └─┘ └─   [0m│" >> /etc/banner\
+echo "╰──────────────────╯" >> /etc/banner\
 ' package/lean/default-settings/files/zzz-default-settings
 
 # Add passwall
