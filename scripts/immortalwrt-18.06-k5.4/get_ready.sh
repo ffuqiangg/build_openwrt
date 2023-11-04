1fd0c3195f7635669bd190d2763239b0cd068360
ffuqiangg <ffuqiangg@gmail.com>
2023-11-04 21:45:23 +0800

    update

--- scripts/immortalwrt-18.06-k5.4/get_ready.sh
+++ scripts/immortalwrt-18.06-k5.4/get_ready.sh
@@ -8,9 +8,9 @@ cd openwrt
 ./scripts/feeds install -a
 
 # Add luci-app-mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-git clone --depth=1 --single-branch -b v5 https://github.com/sbwml/luci-app-mosdns package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# rm -rf feeds/packages/net/v2ray-geodata
+# git clone --depth=1 --single-branch -b v5 https://github.com/sbwml/luci-app-mosdns package/mosdns
+# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # Modify default IP (FROM 192.168.1.1 CHANGE TO 192.168.1.99 )
 sed -i 's/192.168.1.1/192.168.1.99/g' package/base-files/files/bin/config_generate
--- scripts/immortalwrt-master/get_ready.sh
+++ scripts/immortalwrt-master/get_ready.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clone source code
-git clone -q --single-branch -b master --depth 1 https://github.com/immortalwrt/immortalwrt openwrt
+git clone --single-branch -b master --depth 1 https://github.com/immortalwrt/immortalwrt openwrt
 
 cd openwrt
 ./scripts/feeds update -a
@@ -10,9 +10,9 @@ cd openwrt
 # Add luci-app-amlogic
 git clone --depth 1 https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 # Add luci-app-mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-git clone --single-branch -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# rm -rf feeds/packages/net/v2ray-geodata
+# git clone --single-branch -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
