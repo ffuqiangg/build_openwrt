5c9a44cdbc835345818d441e5a90c97bf1e7692b
ffuqiangg <ffuqiangg@gmail.com>
2023-11-11 16:19:12 +0800

    modify banner

--- scripts/immortalwrt-18.06-k5.4/init.sh
+++ scripts/immortalwrt-18.06-k5.4/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Change banner
-echo -e "[34m▍[0mImmortalwrt 18.06 K5.4" > /etc/banner
-echo -e "[34m▍[0mDATE build by ffuqiangg" >> /etc/banner
+echo -e "\n[34m▍[0mImmortalwrt 18.06 K5.4" > /etc/banner
+echo -e "[34m▍[0mDATE build by ffuqiangg\n" >> /etc/banner
--- scripts/immortalwrt-master/init.sh
+++ scripts/immortalwrt-master/init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Change banner
-echo -e "[34m▍[0mImmortalwrt Master" > /etc/banner
-echo -e "[34m▍[0mDATE build by ffuqiangg" >> /etc/banner
+echo -e "\n[34m▍[0mImmortalwrt Master" > /etc/banner
+echo -e "[34m▍[0mDATE build by ffuqiangg\n" >> /etc/banner
+
+# Delete 30-sysinfo.sh
+rm /etc/profile.d/30-sysinfo.sh
 
 # Modify rootfs size on emmc
 sed -i -e '/ROOT1=/c ROOT1=\"720\"' -e '/ROOT2=/c ROOT2=\"720\"' /usr/sbin/openwrt-install-amlogic
