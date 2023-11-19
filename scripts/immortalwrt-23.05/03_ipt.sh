e20e28978507e98902835a2a02865a0471babf18
ffuqiangg <ffuqiangg@gmail.com>
2023-11-19 16:59:32 +0800

    fix bugs

--- scripts/immortalwrt-23.05/03_ipt.sh
+++ scripts/immortalwrt-23.05/03_ipt.sh
@@ -3,6 +3,6 @@
 # 回滚 iptables 为 1.8.7（1.8.8有一个致命bug，当上游升级至1.8.9时可以去除下面3行；具体参见https://www.netfilter.org/projects/iptables/files/changes-iptables-1.8.9.txt）
 rm -rf ./package/network/utils/iptables
 cp -rf ../openwrt_22/package/network/utils/iptables ./package/network/utils/iptables
-cp -rf ../lede/package/network/utils/iptables/patches/900-bcm-fullconenat.patch ./package/network/utils/iptables/patches/900-bcm-fullconenat.patch
+wget https://github.com/coolsnowwolf/lede/raw/master/package/network/utils/iptables/patches/900-bcm-fullconenat.patch -O ./package/network/utils/iptables/patches/900-bcm-fullconenat.patch
 
 exit 0
