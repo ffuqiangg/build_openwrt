55580397288102b4ba6deaf4b1a6f1f7c5f53fd2
ffuqiangg <ffuqiangg@gmail.com>
2023-11-26 20:29:21 +0800

    update

--- scripts/openwrt-23.05/02_prepare_package.sh
+++ scripts/openwrt-23.05/02_prepare_package.sh
@@ -35,12 +35,31 @@ cp -rf ../passwall_pkg/chinadns-ng ./package/chinadns-ng
 # Docker 容器
 rm -rf ./feeds/luci/applications/luci-app-dockerman
 cp -rf ../dockerman/applications/luci-app-dockerman ./feeds/luci/applications/luci-app-dockerman
-sed -i '/auto_start/d' feeds/luci/applications/luci-app-dockerman/root/etc/uci-defaults/luci-app-dockerman
+sed -i -e 's|admin\",|& \"services\",|g' -e 's,Docker,&Man,' -e 's,config\"),overview\"),' ./feeds/luci/luci-app-dockerman/luasrc/controller/dockerman.lua
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/container.lua
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/containers.lua
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/images.lua
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/networks.lua
+sed -i -e 's,admin/,&services/,g' -e 's|admin\",|& \"services\",|g' ./feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/newcontainer.lua
+sed -i -e 's,admin/,&services/,g' -e 's|admin\",|& \"services\",|g' ./feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/newnetwork.lua
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/overview.lua
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/volumes.lua
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/apply_widget.htm
+sed -i -e 's,admin/,&services/,g' -e 's,admin\\/,&services\\/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/container.htm
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/container_file_manager.htm
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/container_stats.htm
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/containers_running_stats.htm
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/images_import.htm
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/images_load.htm
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/newcontainer_resolve.htm
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/overview.htm
+sed -i 's,admin/,&services/,g' ./feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/volume_size.htm
+sed -i '/auto_start/d' ./feeds/luci/applications/luci-app-dockerman/root/etc/uci-defaults/luci-app-dockerman
 pushd feeds/packages
 wget -qO- https://github.com/openwrt/packages/commit/e2e5ee69.patch | patch -p1
 wget -qO- https://github.com/openwrt/packages/pull/20054.patch | patch -p1
 popd
-sed -i '/sysctl.d/d' feeds/packages/utils/dockerd/Makefile
+sed -i '/sysctl.d/d' ./feeds/packages/utils/dockerd/Makefile
 rm -rf ./feeds/luci/collections/luci-lib-docker
 cp -rf ../docker_lib/collections/luci-lib-docker ./feeds/luci/collections/luci-lib-docker
 # DiskMan
