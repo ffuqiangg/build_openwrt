ddc44a76c1f3625a3d7f8481b82b4f6a3e75d11c
ffuqiangg <ffuqiangg@gmail.com>
2023-12-04 16:46:52 +0800

    update

--- patch/openwrt-23.05/etc/uci-defaults/66_init.sh
+++ patch/openwrt-23.05/etc/uci-defaults/66_init.sh
@@ -2,4 +2,11 @@
 
 sed -i -e '/ROOT1=/c\ROOT1=\"720\"' -e '/ROOT2=/c\ROOT2=\"720\"' /usr/sbin/openwrt-install-amlogic
 
+uci set luci.main.lang=zh_cn
+uci commit luci
+
+uci set system.@system[0].timezone=CST-8
+uci set system.@system[0].zonename=Asia/Shanghai
+uci commit system
+
 exit 0
