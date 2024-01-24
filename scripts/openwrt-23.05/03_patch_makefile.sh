df5ec3f5bc4bf8fed9a59aa347c59115ac2d9558
ffuqiangg <ffuqiangg@gmail.com>
2024-01-24 20:00:27 +0800

    update script

--- scripts/openwrt-23.05/03_patch_makefile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-makefile_file="$({ find package -type f | grep Makefile | sed "/Makefile./d"; } 2>"/dev/null")"
-for g in ${makefile_file}; do
-    [ -n "$(grep "golang-package.mk" "$g")" ] && sed -i "s|\.\./\.\.|$\(TOPDIR\)/feeds/packages|g" "$g"
-    [ -n "$(grep "luci.mk" "$g")" ] && sed -i "s|\.\./\.\.|$\(TOPDIR\)/feeds/luci|g" "$g"
-done
-
-exit 0
