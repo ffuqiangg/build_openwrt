644c2206791d344bb613aec52024a35a48cfe92a
ffuqiangg <ffuqiangg@gmail.com>
2024-03-04 16:22:58 +0800

    fix

--- scripts/openwrt-23.05/01_prepare_source_code.sh
+++ scripts/openwrt-23.05/01_prepare_source_code.sh
@@ -29,7 +29,7 @@ clone_repo $docker_lib_repo master docker_lib &
 clone_repo $mosdns_repo v5 mosdns &
 clone_repo $mosdns_pkg master mosdns_pkg &
 clone_repo $sirpdboy_repo main sirpdboy &
-clone_repo $speedtest_repo master netspeedtest &
+clone_repo $netspeedtest_repo master netspeedtest &
 clone_repo $lucidaednext_repo rebase lucidaednext &
 
 wait
