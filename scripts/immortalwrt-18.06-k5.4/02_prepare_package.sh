2d2abf5ffd483c94c93a86ea10a33a01e4a5e07a
ffuqiangg <ffuqiangg@gmail.com>
2024-03-14 16:52:16 +0800

    fix

--- scripts/immortalwrt-18.06-k5.4/02_prepare_package.sh
+++ scripts/immortalwrt-18.06-k5.4/02_prepare_package.sh
@@ -11,9 +11,6 @@ rm ./package/emortal/default-settings/files/openwrt_banner
 sed -i '/openwrt_banner/d' ./package/emortal/default-settings/files/99-default-settings
 sed -i '/etc$/,+2d' ./package/emortal/default-settings/Makefile
 # Mosdns
-rm -rf ./feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
-rm -rf ./feeds/packages/net/mosdns
 rm -rf ./feeds/packages/net/v2ray-geodata
 cp -rf ../mosdns ./package/luci-app-mosdns
 cp -rf ../mosdns_pkg ./package/v2ray-geodata
