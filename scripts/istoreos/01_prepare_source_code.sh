a44b47e1077c287e0c33dfc8a1545d9a4595182c
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2024-04-17 01:56:07 +0800

    Update 01_prepare_source_code.sh

--- scripts/istoreos/01_prepare_source_code.sh
+++ scripts/istoreos/01_prepare_source_code.sh
@@ -23,12 +23,12 @@ clone_repo https://github.com/istoreos/istoreos.git istoreos-22.03 openwrt &
 # clone_repo $openwrt_add_repo master OpenWrt-Add &
 # clone_repo $passwall_pkg_repo main passwall_pkg &
 # clone_repo $passwall_luci_repo main passwall_luci &
-# clone_repo $dockerman_repo master dockerman &
+clone_repo $dockerman_repo master dockerman &
 # clone_repo $diskman_repo master diskman &
 # clone_repo $docker_lib_repo master docker_lib &
 clone_repo $mosdns_repo v5 mosdns &
 # clone_repo $lucidaednext_repo rebase lucidaednext &
-# clone_repo $node_prebuilt_repo packages-23.05 node &
+clone_repo $node_prebuilt_repo packages-22.03 node &
 clone_repo $openclash_repo master openclash &
 # clone_repo $sbwml_openwrt_repo v5 sbwml &
 
