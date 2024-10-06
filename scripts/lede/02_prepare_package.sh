499dcf75661db6e38637bbb1020ca90d9d0b16ab
ffuqiangg <ffuqiangg@gmail.com>
2024-10-06 22:32:55 +0800

    pre-download clash core

--- scripts/lede/02_prepare_package.sh
+++ scripts/lede/02_prepare_package.sh
@@ -62,6 +62,15 @@ sed -i 's,TARGET_CFLAGS += -DHAVE_MAP_SYNC,& -D_LARGEFILE64_SOURCE,' feeds/packa
 # 预配置一些插件
 cp -rf ../patch/files ./files
 sed -i 's,/bin/ash,/bin/bash,' ./package/base-files/files/etc/passwd && sed -i 's,/bin/ash,/bin/bash,' ./package/base-files/files/usr/libexec/login.sh
+mkdir -p files/etc/openclash/core
+pushd files/etc/openclash/core
+clash_version="$(curl -fsSL https://github.com/vernesong/OpenClash/raw/core/master/core_version | sed -n '2p')"
+wget https://github.com/vernesong/OpenClash/raw/core/master/premium/clash-linux-arm64-${clash_version}.gz -O clash_tun.gz && gzip -d clash_tun.gz
+wget https://github.com/vernesong/OpenClash/raw/core/master/meta/clash-linux-arm64.tar.gz -O clash_meta.tar.gz && tar -zxvf clash_meta.tar.gz && mv clash clash_meta
+wget https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-arm64.tar.gz -O clash.tar.gz && tar -zxvf clash.tar.gz
+chmod +x ./clash*
+find ./ -name "*.tar.gz" | xargs rm -f
+popd
 
 chmod -R 755 ./
 find ./ -name *.orig | xargs rm -f
