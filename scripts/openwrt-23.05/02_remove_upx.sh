88a9bab5d7b9033bca86c97e6f72d9366da9a8ec
ffuqiangg <ffuqiangg@gmail.com>
2024-10-09 20:42:13 +0800

    格式化代码

--- scripts/openwrt-23.05/02_remove_upx.sh
+++ scripts/openwrt-23.05/02_remove_upx.sh
@@ -6,6 +6,6 @@
 
 makefile_file="$({ find package | grep Makefile | sed "/Makefile./d"; } 2>"/dev/null")"
 for a in ${makefile_file}; do
-  [ -n "$(grep "upx" "$a")" ] && sed -i "/upx/d" "$a"
+    [ -n "$(grep "upx" "$a")" ] && sed -i "/upx/d" "$a"
 done
 exit 0
