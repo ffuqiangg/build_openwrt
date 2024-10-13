400521defae9291f5278f0058e9cb1aec6023e21
ffuqiangg <ffuqiangg@gmail.com>
2024-10-13 20:20:30 +0800

    测试

--- scripts/istoreos/02_prepare_package.sh
+++ scripts/istoreos/02_prepare_package.sh
@@ -27,6 +27,9 @@ wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Mak
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# Golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 # # Passwall
 # cp -rf ../passwall_luci/luci-app-passwall ./package/new/luci-app-passwall
 # cp -rf ../passwall_pkg ./package/new/passwall-pkg
