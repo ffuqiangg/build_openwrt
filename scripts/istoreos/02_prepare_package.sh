da1453fa7b8c811fd3592035cfe64f1f758bc316
ffuqiangg <ffuqiangg@gmail.com>
2024-10-17 20:11:03 +0800

    istoreos 编译测试

--- scripts/istoreos/02_prepare_package.sh
+++ scripts/istoreos/02_prepare_package.sh
@@ -51,10 +51,6 @@ cp -rf ../immortalwrt_pkg_21/net/simple-obfs ./package/new/simple-obfs
 cp -rf ../immortalwrt_pkg_21/net/chinadns-ng ./package/new/chinadns-ng
 cp -rf ../immortalwrt_pkg_21/devel/gn ./feeds/packages/devel/gn
 ln -sf ../../../feeds/packages/devel/gn ./package/feeds/packages/gn
-# Mosdns
-rm -rf ./feeds/packages/net/v2ray-geodata
-cp -rf ../mosdns ./package/new/luci-app-mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/new/v2ray-geodata
 
 # 预配置一些插件
 cp -rf ../patch/files ./files
