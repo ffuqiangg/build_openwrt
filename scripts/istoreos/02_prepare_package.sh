fd28d7fcb844a5274983dd8b71b2167746016bc4
ffuqiangg <ffuqiangg@gmail.com>
2024-10-19 20:43:11 +0800

    istoreos 编译测试

--- scripts/istoreos/02_prepare_package.sh
+++ scripts/istoreos/02_prepare_package.sh
@@ -8,8 +8,15 @@ sed -i 's/Os/O2/g' include/target.mk
 # 默认开启 Irqbalance
 sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
 
+### 替换准备 ###
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,frp,shadowsocks-libev,v2raya}
+rm -rf feeds/luci/applications/{luci-app-frps,luci-app-frpc,luci-app-v2raya}
+rm -rf feeds/packages/utils/coremark
+
 ### 获取额外的 LuCI 应用和依赖 ###
 mkdir -p ./package/new
+# 替换 coremark
+cp -rf ../openwrt_pkg_ma/utils/coremark ./feeds/packages/utils/coremark
 # 预编译 node
 rm -rf feeds/packages/lang/node
 cp -rf ../node feeds/packages/lang/node
@@ -24,8 +31,8 @@ docker_2_services
 popd
 # DiskMan
 cp -rf ../diskman/applications/luci-app-diskman ./package/new/luci-app-diskman
-mkdir -p package/parted &&
-  wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+mkdir -p package/parted && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
 # Mihomo
 cp -rf ../mihomo ./package/new/mihomo
 # Vsftpd
@@ -44,7 +51,6 @@ popd
 cp -rf ../lede_luci/applications/luci-app-cpufreq ./package/new/luci-app-cpufreq
 sed -i 's,\"system\",\"services\",g' ./package/new/luci-app-cpufreq/luasrc/controller/cpufreq.lua
 # Sing-box
-rm -rf ./feeds/packages/net/sing-box
 cp -rf ../immortalwrt_pkg/net/sing-box ./feeds/packages/net/sing-box
 cp -f ../patch/sing-box/files/sing-box.init ./feeds/packages/net/sing-box/files/sing-box.init
 sed -i '63i\GO_PKG_TARGET_VARS:=$(filter-out CGO_ENABLED=%,$(GO_PKG_TARGET_VARS)) CGO_ENABLED=1\n' ./feeds/packages/net/sing-box/Makefile
@@ -54,8 +60,7 @@ cp -rf ../openwrt_pkg_ma/lang/golang ./feeds/packages/lang/golang
 # Passwall
 cp -rf ../passwall_luci/luci-app-passwall ./package/new/luci-app-passwall
 cp -rf ../passwall_pkg ./package/new/passwall_pkg
-rm -rf ./package/new/passwall-pkg/v2ray-geodata
-rm -rf ./package/new/passwall_pkg/sing-box
+rm -rf ./package/new/passwall_pkg/v2ray_geodata
 # Passwall 白名单
 echo '
 teamviewer.com
@@ -69,15 +74,11 @@ checkipv6.synology.com
 ntp.aliyun.com
 cn.ntp.org.cn
 ntp.ntsc.ac.cn
-' >>./package/new/luci-app-passwall/root/usr/share/passwall/rules/direct_host
+' >> package/new/luci-app-passwall/root/usr/share/passwall/rules/direct_host
 # Mosdns
-rm -rf ./feeds/packages/net/v2ray-geodata
 cp -rf ../mosdns ./package/new/luci-app-mosdns
 cp -rf ../v2ray_geodata ./feeds/packages/net/v2ray-geodata
 # 替换 FRP 内网穿透
-rm -rf ./feeds/luci/applications/luci-app-frps
-rm -rf ./feeds/luci/applications/luci-app-frpc
-rm -rf ./feeds/packages/net/frp
 cp -rf ../immortalwrt_pkg/net/frp ./feeds/packages/net/frp
 sed -i '/etc/d' ./feeds/packages/net/frp/Makefile
 sed -i '/defaults/{N;d;}' ./feeds/packages/net/frp/Makefile
@@ -85,18 +86,16 @@ cp -rf ../lede_luci/applications/luci-app-frps ./feeds/luci/applications/luci-ap
 cp -rf ../lede_luci/applications/luci-app-frpc ./feeds/luci/applications/luci-app-frpc
 # V2raya
 git clone --depth 1 https://github.com/v2rayA/v2raya-openwrt.git luci-app-v2raya
-rm -rf ./feeds/luci/applications/luci-app-v2raya
 cp -rf ./luci-app-v2raya/luci-app-v2raya ./feeds/luci/applications/luci-app-v2raya
 cp -rf ./luci-app-v2raya/v2fly-geodata ./package/new/v2fly-geodata
 rm -rf ./luci-app-v2raya
-rm -rf ./feeds/packages/net/v2raya
 cp -rf ../openwrt_pkg_ma/net/v2raya ./feeds/packages/net/v2raya
 ln -sf ../../../feeds/packages/net/v2raya ./package/feeds/packages/v2raya
 # 添加 rust
-# cp -rf ../immortalwrt_pkg/lang/rust ./feeds/packages/lang/rust
-# ln -sf ../../../feeds/packages/lang/rust ./package/feeds/packages/rust
-# cp -rf ../openwrt_pkg_ma/devel/cargo-c ./feeds/packages/devel/rust
-# ln -sf ../../../feeds/packages/devel/cargo-c ./package/feeds/packages/cargo-c
+cp -rf ../immortalwrt_pkg/lang/rust ./feeds/packages/lang/rust
+ln -sf ../../../feeds/packages/lang/rust ./package/feeds/packages/rust
+cp -rf ../openwrt_pkg_ma/devel/cargo-c ./feeds/packages/devel/rust
+ln -sf ../../../feeds/packages/devel/cargo-c ./package/feeds/packages/cargo-c
 
 # 预配置一些插件
 cp -rf ../patch/files ./files
@@ -105,7 +104,6 @@ mkdir -p files/usr/share/xray
 wget -qO- https://github.com/v2fly/geoip/releases/latest/download/geoip.dat > files/usr/share/xray/geoip.dat
 wget -qO- https://github.com/v2fly/geoip/releases/latest/download/geosite.dat > files/usr/share/xray/geosite.dat
 
-chmod -R 755 ./
 find ./ -name *.orig | xargs rm -f
 find ./ -name *.rej | xargs rm -f
 
