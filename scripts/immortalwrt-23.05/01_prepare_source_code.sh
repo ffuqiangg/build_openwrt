630d0e74f9cf9b4ff8262fe18adc04fd153eb744
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2024-10-27 02:25:19 +0800

    Update 01_prepare_source_code.sh

--- scripts/immortalwrt-23.05/01_prepare_source_code.sh
+++ scripts/immortalwrt-23.05/01_prepare_source_code.sh
@@ -5,7 +5,6 @@ source ./scripts/funcations.sh
 # Clone source code
 latest_release="$(curl -s https://github.com/immortalwrt/immortalwrt/tags | grep -Eo "v[0-9\.]+-*r*c*[0-9]*.tar.gz" | sed -n '/23.05/p' | sed -n 1p | sed 's/.tar.gz//g')"
 clone_repo $immortalwrt_repo ${latest_release} openwrt &
-clone_repo $immortalwrt_repo openwrt-23.05 openwrt_snap &
 clone_repo $openwrt_pkg_repo master openwrt_pkg_ma &
 clone_repo $lede_pkg_repo master lede_pkg &
 clone_repo $mosdns_repo v5 mosdns &
@@ -14,12 +13,6 @@ clone_repo $mihomo_repo main mihomo &
 clone_repo $v2ray_geodata_repo master v2ray_geodata &
 # 等待所有后台任务完成
 wait
-
-# 进行一些处理
-find openwrt/package/* -maxdepth 0 ! -name 'firmware' ! -name 'kernel' ! -name 'base-files' ! -name 'Makefile' -exec rm -rf {} +
-rm -rf ./openwrt_snap/package/firmware ./openwrt_snap/package/kernel ./openwrt_snap/package/base-files ./openwrt_snap/package/Makefile
-cp -rf ./openwrt_snap/package/* ./openwrt/package/
-cp -rf ./openwrt_snap/feeds.conf.default ./openwrt/feeds.conf.default
 
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
