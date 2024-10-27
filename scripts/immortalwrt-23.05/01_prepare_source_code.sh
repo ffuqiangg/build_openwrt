3d04c5786e9ba8a6047838ed9714122aa714d092
ffuqiangg <ffuqiangg@gmail.com>
2024-10-27 22:28:53 +0800

    编译测试

--- scripts/immortalwrt-23.05/01_prepare_source_code.sh
+++ scripts/immortalwrt-23.05/01_prepare_source_code.sh
@@ -6,6 +6,7 @@ source ./scripts/funcations.sh
 latest_release="$(curl -s https://github.com/immortalwrt/immortalwrt/tags | grep -Eo "v[0-9\.]+-*r*c*[0-9]*.tar.gz" | sed -n '/23.05/p' | sed -n 1p | sed 's/.tar.gz//g')"
 clone_repo $immortalwrt_repo ${latest_release} openwrt &
 clone_repo $immortalwrt_repo openwrt-23.05 openwrt_snap &
+clone_repo $openwrt_pkg_repo master openwrt_pkg_ma &
 clone_repo $lede_pkg_repo master lede_pkg &
 clone_repo $mosdns_repo v5 mosdns &
 clone_repo $mihomo_repo main mihomo &
