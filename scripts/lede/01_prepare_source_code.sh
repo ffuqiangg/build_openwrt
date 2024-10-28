3b53beb9a2c5ff2baf7cd2b92d060476784c9823
ffuqiangg <ffuqiangg@gmail.com>
2024-10-28 20:18:37 +0800

    修复

--- scripts/lede/01_prepare_source_code.sh
+++ scripts/lede/01_prepare_source_code.sh
@@ -2,6 +2,7 @@
 
 source ./scripts/funcations.sh
 
+# 开始克隆仓库，并行执行
 clone_repo $lede_repo master openwrt &
 clone_repo $immortalwrt_pkg_repo master immortalwrt_pkg &
 clone_repo $passwall_pkg_repo main passwall_pkg &
@@ -10,10 +11,10 @@ clone_repo $lienol_pkg_repo main lienol_pkg &
 clone_repo $mosdns_repo v5 mosdns &
 clone_repo $openclash_repo master openclash &
 clone_repo $v2ray_geodata_repo master v2ray_geodata &
-
+# 等待所有后台任务完成
 wait
 
-# Modify default IP (FROM 192.168.1.1 CHANGE TO 192.168.1.99 )
+# 修改默认 IP ( 192.168.1.1 改为 192.168.1.99 )
 sed -i 's/192.168.1.1/192.168.1.99/g' openwrt/package/base-files/files/bin/config_generate
 
 exit 0
