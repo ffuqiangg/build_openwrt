8cd4b2a5174281905bb2b00c6071def72a5e4919
ffuqiangg <ffuqiangg@gmail.com>
2024-10-28 14:53:57 +0800

    整理代码

--- scripts/immortalwrt-18.06-k5.4/01_prepare_source_code.sh
+++ scripts/immortalwrt-18.06-k5.4/01_prepare_source_code.sh
@@ -2,15 +2,15 @@
 
 source ./scripts/funcations.sh
 
-# Clone source code
+# 开始克隆仓库，并行执行
 clone_repo $immortalwrt_repo openwrt-18.06-k5.4 openwrt &
 clone_repo $immortalwrt_pkg_repo master immortalwrt_pkg &
 clone_repo $mosdns_repo v5 mosdns &
 clone_repo $v2ray_geodata_repo master v2ray_geodata &
-
+# 等待所有后台任务完成
 wait
 
-# Modify default IP (FROM 192.168.1.1 CHANGE TO 192.168.1.99 )
+# 修改默认 IP 为 192.168.1.99
 sed -i 's/192.168.1.1/192.168.1.99/g' openwrt/package/base-files/files/bin/config_generate
 
 exit 0
--- scripts/immortalwrt-18.06-k5.4/02_prepare_package.sh
+++ scripts/immortalwrt-18.06-k5.4/02_prepare_package.sh
@@ -67,7 +67,6 @@ mkdir -p files/usr/share/xray
 wget -qO- https://github.com/v2fly/geoip/releases/latest/download/geoip.dat > files/usr/share/xray/geoip.dat
 wget -qO- https://github.com/v2fly/geoip/releases/latest/download/geosite.dat > files/usr/share/xray/geosite.dat
 
-chmod -R 755 ./
 find ./ -name *.orig | xargs rm -f
 find ./ -name *.rej | xargs rm -f
 
