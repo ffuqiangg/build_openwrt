a9c6d269246a373a8451354fac12e14a957b5716
ffuqiangg <ffuqiangg@gmail.com>
2024-11-02 15:39:46 +0800

    修改 mmcblk1p3 分区大小

--- scripts/lede/01_prepare_source_code.sh
+++ scripts/lede/01_prepare_source_code.sh
@@ -10,5 +10,7 @@ wait
 
 # 修改默认 IP ( 192.168.1.1 改为 192.168.1.99 )
 sed -i 's/192.168.1.1/192.168.1.99/g' openwrt/package/base-files/luci2/bin/config_generate
+# 修改 mmcblk1p3 分区大小
+sed -i 's/2812/1788/' openwrt/target/linux/amlogic/mesongx/base-files/usr/sbin/install-to-emmc.sh
 
 exit 0
