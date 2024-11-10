d2efe85b73b1d5f0f7b640c57c82a5789e00c87f
ffuqiangg <ffuqiangg@gmail.com>
2024-11-10 14:59:16 +0800

    一些优化

--- scripts/immortalwrt-18.06-k5.4/02_prepare_package.sh
+++ scripts/immortalwrt-18.06-k5.4/02_prepare_package.sh
@@ -52,18 +52,13 @@ popd
 
 # 预配置一些插件
 mkdir -p files
-cp -rf ../files/{etc,/cpufreq/*,net/*} files/
+cp -rf ../files/{etc,immortalwrt-18.06/*,/cpufreq/*,net/*} files/
 mkdir -p files/etc/openclash/core
 clash_version="$(curl -fsSL https://github.com/vernesong/OpenClash/raw/core/master/core_version | sed -n '2p')"
 wget -qO- https://github.com/vernesong/OpenClash/raw/core/master/premium/clash-linux-arm64-${clash_version}.gz | gunzip -c > files/etc/openclash/core/clash_tun
 wget -qO- https://github.com/vernesong/OpenClash/raw/core/master/meta/clash-linux-arm64.tar.gz | tar xOvz > files/etc/openclash/core/clash_meta
 wget -qO- https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-arm64.tar.gz | tar xOvz > files/etc/openclash/core/clash
 chmod +x files/etc/openclash/core/clash*
-wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > files/etc/openclash/GeoIP.dat
-wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat > files/etc/openclash/GeoSite.dat
-mkdir -p files/usr/share/xray
-wget -qO- https://github.com/v2fly/geoip/releases/latest/download/geoip.dat > files/usr/share/xray/geoip.dat
-wget -qO- https://github.com/v2fly/geoip/releases/latest/download/geosite.dat > files/usr/share/xray/geosite.dat
 
 find ./ -name *.orig | xargs rm -f
 find ./ -name *.rej | xargs rm -f
