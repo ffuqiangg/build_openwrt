7885c7444aa0c020d927018b270340bc165e06d1
ffuqiangg <ffuqiangg@gmail.com>
2024-11-15 16:53:33 +0800

    简化编译测试

--- scripts/openwrt-23.05/01_prepare_source_code.sh
+++ scripts/openwrt-23.05/01_prepare_source_code.sh
@@ -5,11 +5,8 @@ source ./scripts/funcations.sh
 # 开始克隆仓库，并行执行
 latest_release="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+-*r*c*[0-9]*.tar.gz" | sed -n '/23.05/p' | sed -n 1p | sed 's/.tar.gz//g')"
 clone_repo $openwrt_repo $latest_release openwrt &
-clone_repo $immortalwrt_repo openwrt-23.05 immortalwrt_23 &
 clone_repo $immortalwrt_pkg_repo master immortalwrt_pkg &
 clone_repo $immortalwrt_luci_repo openwrt-23.05 immortalwrt_luci_23 &
-clone_repo $lede_repo master lede &
-clone_repo $openwrt_repo main openwrt_ma &
 clone_repo $openwrt_pkg_repo master openwrt_pkg_ma &
 clone_repo $openwrt_add_repo master openwrt-add &
 clone_repo $dockerman_repo master dockerman &
@@ -17,6 +14,8 @@ clone_repo $docker_lib_repo master docker_lib &
 clone_repo $mosdns_repo v5 mosdns &
 clone_repo $mihomo_repo main mihomo &
 clone_repo $v2ray_geodata_repo master v2ray_geodata &
+clone_repo $passwall_luci_repo main passwall_luci &
+clone_repo $passwall_pkg_repo main passwall_pkg &
 # 等待所有后台任务完成
 wait
 
--- scripts/openwrt-23.05/02_prepare_package.sh
+++ scripts/openwrt-23.05/02_prepare_package.sh
@@ -21,19 +21,18 @@ patch -p1 < ../../../patch/firewall/04-luci-add-firewall4-nft-rules-file.patch
 popd
 
 ### ADD PKG 部分 ###
-cp -rf ../openwrt-add ./package/new
-rm -rf package/new/{luci-app-mosdns,openwrt_helloworld/v2ray-geodata,OpenWrt-mihomo}
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,frp,microsocks,shadowsocks-libev,zerotier,v2raya,daed}
-rm -rf feeds/luci/applications/{luci-app-frps,luci-app-frpc,luci-app-zerotier}
-rm -rf feeds/packages/utils/coremark
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,microsocks,shadowsocks-libev,v2raya}
 
 ### 获取额外的 LuCI 应用和依赖 ###
-# 更换 Nodejs 版本
+mkdir -p ./package/new
+# 预编译 node
 rm -rf ./feeds/packages/lang/node
 mv ./package/new/feeds_packages_lang_node-prebuilt ./feeds/packages/lang/node
 # 更换 golang 版本
 rm -rf ./feeds/packages/lang/golang
 cp -rf ../openwrt_pkg_ma/lang/golang ./feeds/packages/lang/golang
+# Autocore
+cp -rf ../openwrt-add/autocore-arm ./package/new/autocore-arm
 # mount cgroupv2
 pushd feeds/packages
 patch -p1 < ../../../patch/cgroupfs-mount/0001-fix-cgroupfs-mount.patch
@@ -42,6 +41,33 @@ mkdir -p feeds/packages/utils/cgroupfs-mount/patches
 cp -rf ../patch/cgroupfs-mount/900-mount-cgroup-v2-hierarchy-to-sys-fs-cgroup-cgroup2.patch ./feeds/packages/utils/cgroupfs-mount/patches/
 cp -rf ../patch/cgroupfs-mount/901-fix-cgroupfs-umount.patch ./feeds/packages/utils/cgroupfs-mount/patches/
 cp -rf ../patch/cgroupfs-mount/902-mount-sys-fs-cgroup-systemd-for-docker-systemd-suppo.patch ./feeds/packages/utils/cgroupfs-mount/patches/
+# Passwall
+cp -rf ../passwall_luci/luci-app-passwall ./package/new/luci-app-passwall
+# Passwall 白名单
+echo '
+teamviewer.com
+epicgames.com
+dangdang.com
+account.synology.com
+ddns.synology.com
+checkip.synology.com
+checkip.dyndns.org
+checkipv6.synology.com
+ntp.aliyun.com
+cn.ntp.org.cn
+ntp.ntsc.ac.cn
+' >> package/new/luci-app-passwall/root/usr/share/passwall/rules/direct_host
+# Passwall package
+cp -rf ../passwall_pkg ./package/new/passwall_pkg
+# CPU 占用率限制
+cp -rf ../openwrt-add/luci-app-cpulimit ./package/new/luci-app-cpulimit
+cp -rf ../openwrt-add/imm_pkg/cpulimit ./package/new/cpulimit
+# Filebrowser 文件浏览器
+cp -rf ../immortalwrt_luci_23/applications/luci-app-filebrowser ./package/new/luci-app-filebrowser
+cp -rf ../immortalwrt_pkg/utils/filebrowser ./package/new/filebrowser
+pushd package/new/luci-app-filebrowser
+move_2_services nas
+popd
 # Docker 容器
 rm -rf ./feeds/luci/applications/luci-app-dockerman
 cp -rf ../dockerman/applications/luci-app-dockerman ./feeds/luci/applications/luci-app-dockerman
@@ -50,13 +76,22 @@ rm -rf ./feeds/luci/collections/luci-lib-docker
 cp -rf ../docker_lib/collections/luci-lib-docker ./feeds/luci/collections/luci-lib-docker
 # MosDNS
 cp -rf ../mosdns ./package/new/luci-app-mosdns
+# Mosdns 白名单
+echo 'account.synology.com
+ddns.synology.com
+checkip.synology.com
+checkip.dyndns.org
+checkipv6.synology.com
+ntp.aliyun.com
+cn.ntp.org.cn
+ntp.ntsc.ac.cn' >> package/new/luci-app-mosdns/root/etc/mosdns/rule/whitelist.txt
 cp -rf ../v2ray_geodata ./package/new/v2ray-geodata
 # V2raya
-git clone --depth 1 https://github.com/v2rayA/v2raya-openwrt.git luci-app-v2raya
-cp -rf ./luci-app-v2raya/luci-app-v2raya ./package/new/luci-app-v2raya
-rm -rf ./luci-app-v2raya
+git clone --depth 1 https://github.com/v2rayA/v2raya-openwrt.git v2raya
+cp -rf ./v2raya/luci-app-v2raya ./package/new/luci-app-v2raya
+rm -rf ./v2raya
 cp -rf ../openwrt_pkg_ma/net/v2raya ./feeds/packages/net/v2raya
-# Vsftpd
+# FTP 服务器
 cp -rf ../immortalwrt_luci_23/applications/luci-app-vsftpd ./package/new/luci-app-vsftpd
 cp -rf ../immortalwrt_pkg/net/vsftpd ./package/new/vsftpd
 pushd package/new/luci-app-vsftpd
@@ -72,12 +107,22 @@ cp -rf ../immortalwrt_pkg/net/verysync ./package/new/verysync
 pushd package/new/luci-app-verysync
 move_2_services nas
 popd
+# KMS 服务器
+cp -rf ../immortalwrt_luci_23/applications/luci-app-vlmcsd ./package/new/luci-app-vlmcsd
+cp -rf ../immortalwrt_pkg/net/vlmcsd ./package/new/vlmcsd
 # 晶晨宝盒
 git clone --depth 1 https://github.com/ophub/luci-app-amlogic.git package/new/luci-app-amlogic
+# DDNS scripts
+cp -rf ../openwrt-add/openwrt_pkgs/ddns-scripts-aliyun ./package/new/ddns-scripts-aliyun
 # 翻译及部分功能优化
+cp -rf ../openwrt-add/addition-trans-zh ./package/new/addidtion-trans-zh
 cp -f ../patch/default-settings/openwrt-23.05/zzz-default-settings ./package/new/addition-trans-zh/files/zzz-default-settings
 # mihomo
 cp -rf ../mihomo ./package/new/luci-app-mihomo
+
+### 一些收尾工作 ###
+rm -rf .config
+sed -i 's,CONFIG_WERROR=y,# CONFIG_WERROR is not set,g' target/linux/generic/config-5.15
 
 ### 预配置一些插件 ###
 mkdir -p files
