5cb2a2825b0e9e35a0ba84568910328ea47bf7dd
ffuqiangg <ffuqiangg@gmail.com>
2024-11-19 16:09:05 +0800

    优化代码

--- scripts/immortalwrt-23.05/02_prepare_package.sh
+++ scripts/immortalwrt-23.05/02_prepare_package.sh
@@ -36,7 +36,7 @@ cp -rf ../patch/cgroupfs-mount/902-mount-sys-fs-cgroup-systemd-for-docker-system
 cp -rf ../lede_pkg/net/wget ./feeds/packages/net/wget
 # Mosdns
 cp -rf ../openwrt-apps/luci-app-mosdns ./package/new/luci-app-mosdns
-cp -rf ../openwrt_helloworld/v2ray-geodata ./package/new/v2ray-geodata
+cp -rf ../openwrt-apps/openwrt_helloworld/v2ray-geodata ./package/new/v2ray-geodata
 # Samba4
 sed -i 's,nas,services,g' feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/luci-app-samba4.json
 # Cpufreq
