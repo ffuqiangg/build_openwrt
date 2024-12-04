65142eab0f6edcfa0bf5a0196400b67bd631f46b
ffuqiangg <ffuqiangg@gmail.com>
2024-12-04 20:45:17 +0800

    编译测试

--- scripts/lede/02_prepare_package.sh
+++ scripts/lede/02_prepare_package.sh
@@ -21,6 +21,8 @@ popd
 
 ### 额外的 LuCI 应用和依赖 ###
 mkdir -p package/new
+# 修改 target
+patch -p1 < ../patch/custom_install/custom_target_amlogic_scripts.patch
 # 调整 default settings
 patch -p1 < ../patch/default-settings/lede/default-settings_add_custom_command.patch
 # 预编译 node
