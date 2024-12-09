8afd3dd5ac8b88d24f4954955987145459ecac13
ffuqiangg <ffuqiangg@gmail.com>
2024-12-09 15:44:31 +0800

    优化代码

--- scripts/lede/01_prepare_source_code.sh
+++ scripts/lede/01_prepare_source_code.sh
@@ -13,7 +13,5 @@ wait
 
 # 修改默认 IP ( 192.168.1.1 改为 192.168.1.99 )
 sed -i 's/192.168.1.1/192.168.1.99/g' openwrt/package/base-files/luci2/bin/config_generate
-# 调整分区
-#sed -i 's/2812/1788/' openwrt/target/linux/amlogic/mesongx/base-files/usr/sbin/install-to-emmc.sh
 
 exit 0
