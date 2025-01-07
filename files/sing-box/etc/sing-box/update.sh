3135d84c2e9fbe17524d1621b4e0d1e3d1e2b492
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2025-01-08 00:56:24 +0800

    更新 sing-box

--- files/sing-box/etc/sing-box/update.sh
+++ files/sing-box/etc/sing-box/update.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+. /etc/init.d/sing-box
+url=""
 
-url="" # 订阅地址
-work_dir="/etc/sing-box"
+/etc/init.d/sing-box stop 2>/dev/null
 
-/etc/init.d/sing-box stop
-
-curl --user-agent sing-box --connect-timeout 30 -m 600 -klo ${work_dir}/1.json.new $url
+curl --user-agent sing-box --connect-timeout 30 -m 600 -klo ${RES_DIR}1.json.new $url
 if [ $? -eq 0 ];then
-    mv ${work_dir}/1.json.new ${work_dir}/1.json && jq -s add ${work_dir}/1.json ${work_dir}/template.json > ${work_dir}/config.json
+    mv ${RES_DIR}1.json.new ${RES_DIR}1.json && jq -s add ${RES_DIR}1.json ${RES_DIR}template.json > ${RES_DIR}config.json
 else
-    [ -f ${work_dir}/1.json.new ] && rm -f ${work_dir}/1.json.new
+    [ -f ${RES_DIR}1.json.new ] && rm -f ${RES_DIR}1.json.new
 fi
 
 /etc/init.d/sing-box start
