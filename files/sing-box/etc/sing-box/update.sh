a70957d8969e50a4c5c4f9cb0f0a9933200865b7
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2025-01-08 01:01:32 +0800

    更新 sing-box

--- files/sing-box/etc/sing-box/update.sh
+++ files/sing-box/etc/sing-box/update.sh
@@ -9,6 +9,7 @@ if [ $? -eq 0 ];then
     mv ${RES_DIR}1.json.new ${RES_DIR}1.json && jq -s add ${RES_DIR}1.json ${RES_DIR}template.json > ${RES_DIR}config.json
 else
     [ -f ${RES_DIR}1.json.new ] && rm -f ${RES_DIR}1.json.new
+    echo "Warning! 订阅下载失败，请重试 ..."
 fi
 
 /etc/init.d/sing-box start
