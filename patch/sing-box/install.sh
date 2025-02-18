a07adb9a359814b0b7256d3362784b6328d90123
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2025-02-18 12:18:43 +0800

    修复错误

--- patch/sing-box/install.sh
+++ patch/sing-box/install.sh
@@ -22,7 +22,7 @@ else
     firewall="iptables"; ff="txt"
 fi
 download_dir="https://raw.githubusercontent.com/ffuqiangg/build_openwrt/main/patch/sing-box/$firewall"
-[ -d /etc/sing-box ] && rm -rf /etc/sing-box && mkdir -p /etc/sing-box/$firewall
+[ -d /etc/sing-box ] && rm -rf /etc/sing-box; mkdir -p /etc/sing-box/$firewall
 
 # download
 echo -e "${GREEN_COLOR}INFO${RES} Download Sing-box init ..."
