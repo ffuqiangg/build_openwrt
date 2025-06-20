dda3e176bfc4d465a000f1f75e7841f86a9174cb
ffuqiangg <ffuqiangg@gmail.com>
2025-06-20 20:10:21 +0800

    优化代码

--- scripts/istoreos/02_prepare_package.sh
+++ scripts/istoreos/02_prepare_package.sh
@@ -66,7 +66,7 @@ popd
 # 预配置一些插件
 mkdir -p files
 sed -i 's,/bin/ash,/bin/bash,' ./package/base-files/files/etc/passwd && sed -i 's,/bin/ash,/bin/bash,' ./package/base-files/files/usr/libexec/login.sh
-cp -rf ../files/{init/*,cpufreq/*} files/
+cp -rf ../files/{init/*,cpufreq/*,feeds/*} files/
 mkdir -p files/etc/uci-defaults
 cp -f ../patch/default-settings/istoreos/zzz-default-settings ./files/etc/uci-defaults/
 mkdir -p files/etc/openclash/core
