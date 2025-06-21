ea18ae21d935bf1304545847f4c1d59722804193
ffuqiangg <ffuqiangg@gmail.com>
2025-06-21 21:06:24 +0800

    清理冗余代码

--- scripts/immortalwrt-24.10/01_prepare_source_code.sh
+++ scripts/immortalwrt-24.10/01_prepare_source_code.sh
@@ -4,7 +4,6 @@
 
 # Clone source code
 clone_repo $immortalwrt_repo ${1} openwrt &
-# clone_repo $immortalwrt_repo openwrt-24.10 openwrt_snap &
 clone_repo $openwrt_pkg_repo master openwrt_pkg_ma &
 clone_repo $lede_pkg_repo master lede_pkg &
 clone_repo $node_prebuilt_repo packages-24.10 node &
@@ -12,12 +11,6 @@ clone_repo $openwrt_apps_repo main openwrt-apps &
 clone_repo $amlogic_repo main amlogic &
 # 等待所有后台任务完成
 wait
-
-# 进行一些处理
-# find openwrt/package/* -maxdepth 0 ! -name 'firmware' ! -name 'kernel' ! -name 'base-files' ! -name 'Makefile' -exec rm -rf {} +
-# rm -rf ./openwrt_snap/package/firmware ./openwrt_snap/package/kernel ./openwrt_snap/package/base-files ./openwrt_snap/package/Makefile
-# cp -rf ./openwrt_snap/package/* ./openwrt/package/
-# cp -rf ./openwrt_snap/feeds.conf.default ./openwrt/feeds.conf.default
 
 # 设置默认密码为 password
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
