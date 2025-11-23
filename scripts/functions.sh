912eaf8d5c1f58b926fda6a6739a91853b043629
ffuqiangg <ffuqiangg@gmail.com>
2025-11-23 22:21:01 +0800

    优化代码

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-openwrt_repo="https://github.com/openwrt/openwrt.git"
-openwrt_pkg_repo="https://github.com/openwrt/packages.git"
-openwrt_luci_repo="https://github.com/openwrt/luci.git"
-immortalwrt_repo="https://github.com/immortalwrt/immortalwrt.git"
-immortalwrt_pkg_repo="https://github.com/immortalwrt/packages.git"
-immortalwrt_luci_repo="https://github.com/immortalwrt/luci.git"
-lede_repo="https://github.com/coolsnowwolf/lede.git"
-lede_pkg_repo="https://github.com/coolsnowwolf/packages.git"
-lede_luci_repo="https://github.com/coolsnowwolf/luci.git"
-istoreos_repo="https://github.com/istoreos/istoreos.git"
+openwrt_repo="https://github.com/openwrt/openwrt"
+openwrt_pkg_repo="https://github.com/openwrt/packages"
+openwrt_luci_repo="https://github.com/openwrt/luci"
+immortalwrt_repo="https://github.com/immortalwrt/immortalwrt"
+immortalwrt_pkg_repo="https://github.com/immortalwrt/packages"
+immortalwrt_luci_repo="https://github.com/immortalwrt/luci"
+lede_repo="https://github.com/coolsnowwolf/lede"
+lede_pkg_repo="https://github.com/coolsnowwolf/packages"
+lede_luci_repo="https://github.com/coolsnowwolf/luci"
+istoreos_repo="https://github.com/istoreos/istoreos"
 lienol_pkg_repo="https://github.com/Lienol/openwrt-package"
-openwrt_apps_repo="https://github.com/ffuqiangg/openwrt_apps.git"
+openwrt_apps_repo="https://github.com/ffuqiangg/openwrt_apps"
 passwall_pkg_repo="https://github.com/xiaorouji/openwrt-passwall-packages"
 passwall_luci_repo="https://github.com/xiaorouji/openwrt-passwall"
 dockerman_repo="https://github.com/lisaac/luci-app-dockerman"
 diskman_repo="https://github.com/lisaac/luci-app-diskman"
 docker_lib_repo="https://github.com/lisaac/luci-lib-docker"
-mosdns_repo="https://github.com/sbwml/luci-app-mosdns.git"
-openclash_repo="https://github.com/vernesong/OpenClash.git"
+mosdns_repo="https://github.com/sbwml/luci-app-mosdns"
+openclash_repo="https://github.com/vernesong/OpenClash"
 node_prebuilt_repo="https://github.com/sbwml/feeds_packages_lang_node-prebuilt"
-nikki_repo="https://github.com/nikkinikki-org/OpenWrt-nikki.git"
-momo_repo="https://github.com/nikkinikki-org/OpenWrt-momo.git"
+nikki_repo="https://github.com/nikkinikki-org/OpenWrt-nikki"
+momo_repo="https://github.com/nikkinikki-org/OpenWrt-momo"
 v2ray_geodata_repo="https://github.com/sbwml/v2ray-geodata"
-amlogic_repo="https://github.com/ophub/luci-app-amlogic.git"
+amlogic_repo="https://github.com/ophub/luci-app-amlogic"
 
 clone_repo ()
 {
     repo_url=$1
     branch_name=$2
     target_dir=$3
-    git clone -b $branch_name --depth 1 $repo_url $target_dir
+    git clone --single-branch -b $branch_name --depth 1 $repo_url $target_dir
 }
 
 move_2_services ()
