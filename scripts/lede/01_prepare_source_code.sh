030facdd69f4c4067e7d7a87ee0b47ca77216f3d
ffuqiangg <ffuqiangg@gmail.com>
2025-11-28 20:59:18 +0800

    测试

--- scripts/lede/01_prepare_source_code.sh
+++ scripts/lede/01_prepare_source_code.sh
@@ -16,6 +16,6 @@ sed -i 's/192.168.1.1/192.168.1.99/g' openwrt/package/base-files/luci/bin/config
 sed -i '/wireless/d' openwrt/package/lean/default-settings/files/zzz-default-settings
 sed -Ei "s/(disabled=)0/\11/" openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 调整内核版本为 5.15
-sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=5.15/' openwrt/target/linux/amlogic/Makefile
+#sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=5.15/' openwrt/target/linux/amlogic/Makefile
 
 exit 0
