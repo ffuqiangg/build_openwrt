80bb5ed64e5156b7c796bcefde4c8796bf3f4803
ffuqiangg <ffuqiangg@gmail.com>
2026-01-09 15:58:09 +0800

    去除不必要依赖避免报错

--- scripts/istoreos/02_prepare_package.sh
+++ scripts/istoreos/02_prepare_package.sh
@@ -84,6 +84,7 @@ rm -rf feeds/packages/net/{shadowsocks-libev,v2ray-core,xray-core}
 cp -rf ../openwrt-add/openwrt_helloworld ./package/new/
 rm -rf ./package/new/openwrt-add/openwrt_helloworld/v2ray-geodata
 sed -i '/select PACKAGE_geoview/{n;s/default n/default y/;}' package/new/openwrt_helloworld/luci-app-passwall/Makefile
+sed -i 's, +libopenssl-legacy,,g' package/new/openwrt_helloworld/shadowsocksr-libev/Makefile
 # V2raya
 rm -rf ./feeds/luci/applications/luci-app-v2raya ./feeds/packages/net/v2raya
 cp -rf ../immortalwrt_luci_ma/applications/luci-app-v2raya ./feeds/luci/applications/luci-app-v2raya
