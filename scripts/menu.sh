bc784059298cc03cff93c04feae3181720de8517
ffuqiangg <ffuqiangg@gmail.com>
2026-01-31 16:49:37 +0800

    fix

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -9,10 +9,10 @@ for a in ${resource_file}; do
     [ -n "$(grep '"VPN"' "$a")" ] && sed -i 's,"VPN","Services",g' "$a"
     [ -n "$(grep '\[\[vpn\]\]' "$a")" ] && sed -i 's,\[\[vpn\]\],\[\[services\]\],g' "$a"
     [ -n "$(grep 'admin/vpn' "$a")" ] && sed -i 's,admin/vpn,admin/services,g' "$a"
-    [ -n "$(grep '"nas"' "$a")" ] && sed -i "s,\"nas\",\"services\",g" "$a"
-    [ -n "$(grep '"NAS"' "$a")" ] && sed -i "s,\"NAS\",\"Services\",g" "$a"
-    [ -n "$(grep "\[\[nas\]\]" "$a")" ] && sed -i "s,\[\[nas\]\],\[\[services\]\],g" "$a"
-    [ -n "$(grep "admin/nas" "$a")" ] && sed -i "s,admin/nas,admin/services,g" "$a"
+    [ -n "$(grep '"nas"' "$a")" ] && sed -i 's,"nas","services",g' "$a"
+    [ -n "$(grep '"NAS"' "$a")" ] && sed -i 's,"NAS","Services",g' "$a"
+    [ -n "$(grep '\[\[nas\]\]' "$a")" ] && sed -i 's,\[\[nas\]\],\[\[services\]\],g' "$a"
+    [ -n "$(grep 'admin/nas' "$a")" ] && sed -i 's,admin/nas,admin/services,g' "$a"
 done
 
 exit 0
