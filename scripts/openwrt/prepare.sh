a2cb1d7378e1349ec087cc88f6863ad8f084d2c3
ffuqiangg <ffuqiangg@gmail.com>
2026-02-21 13:55:25 +0800

    update

--- scripts/openwrt/prepare.sh
+++ scripts/openwrt/prepare.sh
@@ -81,8 +81,6 @@ p "更新 Feeds"
 
 p "卸载无法编译的包"
 ./scripts/feeds uninstall onionshare-cli luci-app-mjpg-streamer || true
-p "修复 zabbix 依赖，仅针对当前版本"
-wget https://github.com/openwrt/packages/raw/refs/heads/master/admin/zabbix/Makefile -O ./feeds/packages/admin/zabbix/Makefile
 p "取消 attendedsysupgrade"
 sed -i '/attendedsysupgrade/d' ${wrtdir}/feeds/luci/collections/luci-nginx/Makefile
 
